1976488e00bad713da716b2afee3cbbd826f970b
Concha Asensio <conchaasensiomr@gmail.com>
2021-01-09 14:14:35 +0100

    add messages to confirm whether the selected circle is the same as the highlighted one

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const playButton = document.querySelector('.js-button');
-
 let circles = document.querySelectorAll('.js-circle');
+
 function highlightCircle(position) {
   circles[position].classList.add('js-highlighted-circle');
 }
@@ -11,9 +11,23 @@ function clearHighlightedCircles() {
   circles.forEach((circle) => circle.classList.remove('js-highlighted-circle'));
 }
 
-function handleHighlightCircle() {
-  highlightCircle(Math.floor(Math.random() * 4));
+let highlightedCircle;
+function handlePlayButton() {
+  highlightedCircle = Math.floor(Math.random() * 4);
+  highlightCircle(highlightedCircle);
   setTimeout(clearHighlightedCircles, 1000);
 }
 
-playButton.addEventListener('click', handleHighlightCircle);
+function handleSelectedCircle(event) {
+  let element = event.currentTarget;
+  if (element === circles[highlightedCircle]) {
+    alert('Has acertado');
+  } else {
+    alert('Te has equivocado');
+  }
+}
+
+playButton.addEventListener('click', handlePlayButton);
+circles.forEach((circle) =>
+  circle.addEventListener('click', handleSelectedCircle)
+);
