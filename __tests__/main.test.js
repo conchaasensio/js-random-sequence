adb24edaf44f7f8a9449b24779dbdc866c1bceb0
Concha Asensio <conchaasensiomr@gmail.com>
2021-01-10 16:31:32 +0100

    test that checks that a circle should be highlighted after clicking play button

--- __tests__/main.test.js
+++ __tests__/main.test.js
@@ -1,6 +1,9 @@
-import '@testing-library/jest-dom';
 import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 import '../main';
+
+jest.useFakeTimers();
 
 test('should render the play button', async () => {
   const button = await screen.getByRole('button');
@@ -13,4 +16,16 @@ test('should render four circles', async () => {
   const circlesContainer = await screen.getByTestId('circles-container');
 
   expect(circlesContainer.childNodes.length).toBe(4);
+});
+
+test('should highlight a circle on play button click', async () => {
+  const button = screen.getByRole('button');
+  Math.random = () => 0;
+
+  await userEvent.click(button);
+
+  const timeBeforeStartingHighlightCircles = 1000;
+  jest.advanceTimersByTime(timeBeforeStartingHighlightCircles);
+  const circlesContainer = await screen.getByTestId('circles-container');
+  expect(circlesContainer.childNodes[0]).toHaveClass('js-highlighted-circle');
 });
